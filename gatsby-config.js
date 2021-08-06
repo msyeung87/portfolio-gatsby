f600a677b610222dd224ea5b494942f6a2ddec2a
Matthew Yeung <matthew@Matthews-MacBook-Pro.local>
2021-08-06 18:13:24 -0400

    add netlify plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,7 @@ module.exports = {
     `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-netlify`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
